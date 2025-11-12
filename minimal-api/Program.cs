b44ff319f7888594ec5c9cdbddcce6b5a27f4711
Luizx0 <Loue0982@gmail.com>
2025-11-12 12:08:13 -0300

    modified:   minimal-api/Program.cs

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -3,4 +3,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@exmplo.com" && loginDTO.Password == "123456")
+    {
+        return Results.Ok("Login com sucesso!");
+    }
+    else
+        return Results.Unauthorized();
+});
+
+
+public class LoginDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+
 app.Run();
